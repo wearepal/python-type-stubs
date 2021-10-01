63b7ae59752c3cbccc17361149197024724bd9df
Thomas M <thomas5max@gmail.com>
2021-10-01 13:46:33 +0100

    [setup.py] Remove PIL, add cv2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="python-type-stubs",
-    version="0.1.0.dev0",
+    version="0.1.1.dev0",
     url="https://github.com/predictive-analytics-lab/python-type-stubs",
     author="microsoft",
     description="A set of type stubs for popular Python packages.",
@@ -15,15 +15,15 @@ setup(
     long_description_content_type="text/markdown",
     package_dir={  # this tells distutils that pandas-stubs is in pandas
         "matplotlib-stubs": "matplotlib",
-        "PIL-stubs": "PIL",
+        "cv2-stubs": "cv2",
         "pandas-stubs": "pandas",
     },
     package_data={
         "matplotlib-stubs": ["*.pyi", "**/*.pyi", "**/**/*.pyi", "**/**/**/*.pyi"],
-        "PIL-stubs": ["*.pyi", "**/*.pyi", "**/**/*.pyi", "**/**/**/*.pyi"],
+        "cv2-stubs": ["*.pyi", "**/*.pyi", "**/**/*.pyi", "**/**/**/*.pyi"],
         "pandas-stubs": ["*.pyi", "**/*.pyi", "**/**/*.pyi", "**/**/**/*.pyi"],
     },
-    packages=["matplotlib-stubs", "PIL-stubs", "pandas-stubs"],
+    packages=["matplotlib-stubs", "cv2-stubs", "pandas-stubs"],
     python_requires=">=3.6",
     extras_require={},
     classifiers=[  # classifiers can be found here: https://pypi.org/classifiers/
